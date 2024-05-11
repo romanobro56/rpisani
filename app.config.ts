535ae921f7470d4ed3695d40b2b6f2701d6c0973
Roman Pisani <romanp2929@gmail.com>
2024-05-11 16:40:25 -0400

    random plugin config try number 2

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "@solidjs/start/config";
-import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  vite: {
-    plugins: [solidPlugin()],
-  },
   server: {
     preset: "vercel"
   }
