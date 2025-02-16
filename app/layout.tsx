fb7dbbeb7f70a1e9358e572a48183111c75a33fb
Roman Pisani <romanp2929@gmail.com>
2025-02-16 16:50:34 -0500

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/react"
+
 export const metadata: Metadata = {
   title: "Roman Pisani",
   description: "Roman Pisani's Site",
